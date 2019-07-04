406d90a12221385fb012ab9bdda9e858915ede22
Kyung Lee <kyung.lee@softwire.com>
2019-07-04 17:29:25 +0100

    Lukas fixed everything (hopefully)

--- CSVParser.js
+++ CSVParser.js
@@ -4,70 +4,73 @@ const filename = 'books.csv';
 const filepath = path.join(__dirname, './', filename);
 let ISBNList = [];
 const database = require('./DataBase').DataBase;
+const parse  = require("csv-parse/lib/sync");
 
 main();
 
 function readFile() {
-    return fs.readFileSync(filepath, { encoding: 'utf-8' }).split("\n");
+    return fs.readFileSync(filepath, { encoding: 'utf-8' });
 }
 
-function processFile(rawInputData) {
+async function processFile(rawInputData) {
     database.sequelize.sync();
 
-    let header = rawInputData[0].toLowerCase().replace("\"", "").split(',');
-    let authorIndex = header.findIndex((element) => element == "author");
-    let publisherIndex = header.findIndex((element) => element == "publisher");
-    let ISBNIndex = header.findIndex((element) => element == "isbn");
-    let titleIndex = header.findIndex((element) => element == "title");
+    let line = parse(rawInputData);
 
-    for (i = 1; i < rawInputData.length; i++) {
-        let processedData = rawInputData[i].split(',');
-        bookInfo = [new Promise((resolve) => { resolve(null); }), new Promise((resolve) => { resolve(null); })];
+    let authorIndex = line[0].findIndex((element) => element == "author");
+    let publisherIndex = line[0].findIndex((element) => element == "publisher");
+    let ISBNIndex = line[0].findIndex((element) => element == "isbn");
+    let titleIndex = line[0].findIndex((element) => element == "title");
+
+    for (i = 1; i < line.length; i++) {
+        let processedData = line[i];
+        bookInfo = [[{ id: null }], [{ id: null }]];
+
+        console.log(processedData);
+        console.log(authorIndex, publisherIndex, ISBNIndex, titleIndex);
 
         if (processedData.length == 0) {
             continue;
         }
 
         if (processedData[authorIndex] != undefined) {
-            bookInfo[0] = (database.authors.findOrCreate({
+            bookInfo[0] = await (database.authors.findOrCreate({
                 where: { name: processedData[authorIndex] },
                 defaults: { name: processedData[authorIndex] }
             }))
         }
 
         if (processedData[publisherIndex] != undefined) {
-            bookInfo[1] = (database.authors.findOrCreate({
+            bookInfo[1] = await (database.authors.findOrCreate({
                 where: { name: processedData[publisherIndex] },
                 defaults: { name: processedData[publisherIndex] }
             }))
         }
 
-        Promise.all(bookInfo).then(([authorIdNew, publisherIdNew]) => {
+        if (processedData[ISBNIndex] == undefined || isNaN(processedData[ISBNIndex]) || processedData[ISBNIndex].length < 5) {
+            continue;
+        }
 
-            if (processedData[ISBNIndex] == undefined || isNaN(processedData[ISBNIndex]) || processedData[ISBNIndex].length < 5) {
-                return;
+        console.log(processedData[ISBNIndex], processedData[titleIndex]);
+        console.log(bookInfo[0][0].id, bookInfo[1][0].id);
+
+        let ISBNId = await database.book_temps.findOrCreate({
+            where: {
+                ISBN: processedData[ISBNIndex],
+            },
+            defaults: {
+                ISBN: processedData[ISBNIndex],
+                title: processedData[titleIndex],
+                authorId: bookInfo[0][0].id,
+                publisherId: bookInfo[1][0].id,
             }
 
+        });
 
-            database.book_temps.findOrCreate({
-                where: {
-                    ISBN: processedData[ISBNIndex],
-                    title: processedData[titleIndex],
-                    authorId: authorIdNew[0].id,
-                    publisherId: 2,
-                },
-                defaults: {
-                    ISBN: processedData[ISBNIndex],
-                    title: processedData[titleIndex],
-                    authorId: authorIdNew[0].id,
-                    publisherId: 2,
-                }
-            }).then(
-                database.books.create({
-                    bookTempISBN: processedData[ISBNIndex]
-                })
-            );
+        await database.books.create({
+            bookTempISBN: processedData[ISBNIndex]
         })
+
     }
     database.sequelize.sync();
 }
